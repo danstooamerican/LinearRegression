582b4468413b40f25e9af5c3af334610992f939c
Daniel Augustin <uzhpf@student.kit.edu>
2019-10-22 20:56:26 +0200

    added output option for the loaded data

--- main.c
+++ main.c
@@ -2,9 +2,37 @@
 #include <stdlib.h>
 #include "input/fileReader.h"
 
+void printValues(DataArray values) {
+    const char LIST_OPTION = 'l';
+    printf("%i value pairs have been loaded. Press '%c' to display a list or hit enter.\n", values.size, LIST_OPTION);
+
+    char c;
+    c = getchar();
+
+    printf("\n");
+
+    if (c == LIST_OPTION) {
+        DataPoint *array = values.array;
+        for (int i = 0; i < values.size; i++) {
+            printf("%lf : %lf\n", array[i].x, array[i].y);
+        }
+    }
+
+    printf("\n");
+}
+
 /*
     The user passes one argument to the program.
-    arg[1]: Full path to a text file including a list of 
+    arg[1]: Name of a text file which is located in the program folder.
+
+    Text file format:
+    xValue1 yValue1
+           .
+           .
+           .
+    xValueN yValueN
+
+    Values are decimal numbers like -1, 42, 5.1, 7.0012
 */
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -15,8 +43,11 @@ int main(int argc, char** argv) {
 
     const int DATA_PATH = 1;
     
-    DataPoint *values;
+    DataArray values;
     values = readFile(argv[DATA_PATH]);
+
+    printValues(values);
 
     return EXIT_SUCCESS;
 }
+
